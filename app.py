c7af0d700d91f3307af548666083c0413e63d1dd
Allan Elsberry <allan.elsberry@gmail.com>
2019-07-17 16:39:52 -0400

    fixed parking checkbox issue

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ class Jet(db.Model):
         return str(self.side)
 
 def seed_db ():
-    for i in range(8):
-        new_jet = Jet(id=i, side=randint(400,700),parking=randint(1,24), fuel=choice([True,False]),
+    for i in range(1,15):
+        new_jet = Jet(id=i, side=randint(400,700),parking=randint(1,num_parking_spots), fuel=choice([True,False]),
         dta=choice([True,False]),flying=choice([True,False]))
         db.session.add(new_jet)
         db.session.commit()
@@ -58,18 +58,24 @@ def fill_parking():
             
     return park_list
 
+def turn_bool(my_string):
+    if my_string in ['off','false',None,'none','None','False']:
+        return False
+    return True
 
 ## Make the DB table (if it hasnt been created)
 db.drop_all()
 db.create_all()
 seed_db()
 
+
+## Only shows the cover page for the site
 @app.route('/')
 @app.route('/schedule')
 def hello_world():
     return render_template('index.html')
 
-
+## Lists the parking spots available and fills in jets
 @app.route('/parking')
 def parking_map():
     return render_template('parking.html',jets=fill_parking())
@@ -83,8 +89,8 @@ def jet_list():
 @app.route('/jets/add', methods=['POST'])  
 def add_jet():      
     ## Make a new jet and insert into database
-
     return redirect("/edit/" + str(len(jets)-1))
+
 
 @app.route("/remove/<i>",methods=['GET'])
 def remove_jet(i):
@@ -94,33 +100,57 @@ def remove_jet(i):
 
 @app.route("/fly/<i>",methods=['GET'])
 def fly_jet(i):
-        ## Markt the selected jet as Flying
-
+    ## Mark the selected jet as Flying
+    Jet.query.get(int(i)).flying = True
     return redirect("/parking")
+
+@app.route("/land/<i>",methods=['GET'])
+def land_jet(i):
+    ## Mark the selected jet as Flying
+    Jet.query.get(int(i)).flying = False
+    return redirect("/parking")
+
 
 @app.route("/park/<i>",methods=['POST'])
 def park_jet(i):
-    global jets
-    jets[int(request.form.get("serial_landed"))].update({'parking':int(i)})
-    jets = order_jets(jets)
-    save_jets(jets)
+    ## Get the id of the jet that was parked and update the database
+    Jet.query.get(int(request.form.get("id_landed"))).parking = int(i)
     return redirect("/parking")
+
+@app.route("/park_edit/<i>",methods=['POST'])
+def park_edit(i):
+    ## Get the id and info from the parking form and update database
+
+    print(request.form)
+
+    if request.form.get("fuel"):
+        Jet.query.get(int(i)).fuel = True
+    else:
+        Jet.query.get(int(i)).fuel = False
+    if request.form.get("dta"):
+        Jet.query.get(int(i)).dta = True
+    else:
+        Jet.query.get(int(i)).dta = False
+    if request.form.get("status"):
+        Jet.query.get(int(i)).status = True
+    else:
+        Jet.query.get(int(i)).status = False
+
+
+    return redirect("/parking")
+
+
+
+
+
 
 
 
 @app.route("/edit/<i>",methods=['POST','GET'])
 def edit_jet(i):
-    global jets
     if request.method == 'GET':
         return render_template('edit.html', jet=jets[int(i)],index = int(i))
     if request.method == 'POST':
-        jets[int(i)]={
-            "side":int(request.form.get("side")),
-            "parking":int(request.form.get("parking")),
-            "fuel":float(request.form.get("fuel")),
-            "ordnance":request.form.get("ordnance"),
-            "up":bool(request.form.get("up"))
-        }
 
         return redirect("/jets")
 
