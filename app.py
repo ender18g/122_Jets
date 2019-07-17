40db532bdc8b357207eb4bf4a520f58cbfb1415d
Allan Elsberry <allan.elsberry@gmail.com>
2019-07-17 14:44:07 -0400

    added sql database

--- app.py
+++ app.py
@@ -1,65 +1,68 @@
 from flask import Flask, render_template, request, redirect
-import json
+from flask_sqlalchemy import SQLAlchemy
+from random import randint, choice
+import os
+
 app = Flask(__name__)
-app.testing = True
+num_parking_spots = 8*3
 
-def load_jets():
-    try: 
-        with open("jets.txt","r") as data_file:
-            jets = json.load(data_file)
-    except:
-        print("loading DEFAULT JETS")
-        jets = [
-            {'side':124, 'parking':1, 'fuel':15.1, 'ordnance':'SCL-3', 'up':True},
-            {'side':123, 'parking':5, 'fuel':16.1, 'ordnance':'SCL-2', 'up':True},
-            {'side':126, 'parking':7, 'fuel':4.1, 'ordnance':'SCL-3', 'up':True},
-            {'side':102, 'parking':3, 'fuel':15.4, 'ordnance':'SCL-4', 'up':True},
-            {'side':134, 'parking':2, 'fuel':3.1, 'ordnance':'SCL-3', 'up':False},
-            {'side':155, 'parking':12, 'fuel':15.1, 'ordnance':'SCL-3', 'up':True}
-        ]
-    return jets
+##Setup the Database:
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
 
-def save_jets(jets):
-    with open("jets.txt", "w") as data_file:
-        json.dump(jets, data_file, indent=2)
-    return None
+## Setup the JET Class / Table in the Database:
 
-def order_jets(jet_list):
-    global jets
-    jets = sorted(jet_list,key=lambda i: i['side'])
-    for i in range(len(jets)):
-        jets[i].update({"serial":i})
-    return jets
 
-def fill_jets(jet_list):
-    jet_list = jet_list[:]
-    parked_jets = []
-    flying_jets = []
+class Jet(db.Model):
+    __tablename__ = 'jets'
+    id = db.Column(db.Integer, primary_key=True)
+    side = db.Column(db.Integer, unique=True, index=True)
+    parking = db.Column(db.Integer, default = 0)
+    fuel = db.Column(db.Boolean, default = True)
+    dta = db.Column(db.Boolean, default= True)
+    notes = db.Column(db.String(512), nullable=True)
+    flying = db.Column(db.Boolean, default = False)
+    ordnance = db.Column(db.String(64), default="SCL-0")
+    status = db.Column(db.Boolean, default = True)
 
-    for jet in jet_list:
-        if jet['parking']==0:
-            flying_jets.append(jet)
-        else:
-            parked_jets.append(jet)
-    
+    def __repr__(self):
+        return str(self.side)
+
+def seed_db ():
+    for i in range(8):
+        new_jet = Jet(id=i, side=randint(400,700),parking=randint(1,24), fuel=choice([True,False]),
+        dta=choice([True,False]),flying=choice([True,False]))
+        db.session.add(new_jet)
+        db.session.commit()
+    return True
+
+def get_jets ():
+    return Jet.query.all()
+
+def fill_parking():
+    jet_list = get_jets()
+    park_list = []
     exists = False
-    for i in range(1,31):
-        for jet in parked_jets:
-            if jet['parking']==i:
-                exists = True
+    for i in range(1,num_parking_spots+1):
+        for jet in jet_list:
+            if jet.parking == i:
+                exists=True
+                park_list.append(jet)
                 break
         if not exists:
-            parked_jets.append({'side':'-','parking':i})
+            park_list.append({"side":"-","parking":i})
         exists = False
-
-    parked_jets.sort(key=lambda i: i['parking'])
-    flying_jets
-
-    return [parked_jets,flying_jets]
+            
+    return park_list
 
 
-jets = load_jets()
-jets = order_jets(jets)
+## Make the DB table (if it hasnt been created)
+db.drop_all()
+db.create_all()
+seed_db()
 
 @app.route('/')
 @app.route('/schedule')
@@ -69,31 +72,30 @@ def hello_world():
 
 @app.route('/parking')
 def parking_map():
-    [parked_jets,flying_jets] = fill_jets(jets)
-    return render_template('parking.html',jets=parked_jets, all_jets = jets)
+    return render_template('parking.html',jets=fill_parking())
 
 
 @app.route('/jets', methods=['GET'])
 def jet_list():
     if request.method == 'GET':
-        return render_template('jets.html', jets=jets )
+        return render_template('jets.html', jets=get_jets() )
 
 @app.route('/jets/add', methods=['POST'])  
 def add_jet():      
-    jets.append({'side':'000', 'parking':0, 'fuel':0, 'ordnance':'SCL-3', 'up':False})
+    ## Make a new jet and insert into database
+
     return redirect("/edit/" + str(len(jets)-1))
 
 @app.route("/remove/<i>",methods=['GET'])
 def remove_jet(i):
-    jets.pop(int(i))
-    return render_template('jets.html', jets=jets )
+    ## remove the selected jet and delete from the database
+
+    return render_template('jets.html', jets=get_jets() )
 
 @app.route("/fly/<i>",methods=['GET'])
 def fly_jet(i):
-    global jets
-    jets[int(i)].update({'parking':0})
-    jets = order_jets(jets)
-    save_jets(jets)
+        ## Markt the selected jet as Flying
+
     return redirect("/parking")
 
 @app.route("/park/<i>",methods=['POST'])
@@ -119,8 +121,7 @@ def edit_jet(i):
             "ordnance":request.form.get("ordnance"),
             "up":bool(request.form.get("up"))
         }
-        jets = order_jets(jets)
-        save_jets(jets)
+
         return redirect("/jets")
 
 
